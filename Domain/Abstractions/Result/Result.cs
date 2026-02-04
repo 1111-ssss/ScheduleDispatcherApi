8df15fe1f6c6fc595134b2ed6711ee891d8f6e09
1111-ssss <gurskievgeni@gmail.com>
2026-02-04 09:21:01 +0300

    feat(result): add details

--- Domain/Abstractions/Result/Result.cs
+++ Domain/Abstractions/Result/Result.cs
@@ -3,49 +3,72 @@ namespace Domain.Abstractions.Result;
 public interface IResultBase
 {
     bool IsSuccess { get; }
+    bool IsComplited { get; }
     ErrorCode? Error { get; }
     string? Message { get; }
+    Dictionary<string, string>? Details { get; }
 }
 public sealed class Result : IResultBase
 {
     public bool IsSuccess => Error is null;
+    public bool IsComplited => IsSuccess;
     public ErrorCode? Error { get; }
     public string? Message { get; }
-    internal Result(ErrorCode? errorCode = null, string? message = null)
+    public Dictionary<string, string>? Details { get; }
+    internal Result(
+        ErrorCode? errorCode = null,
+        string? message = null,
+        Dictionary<string, string>? details = null
+    )
     {
         Error = errorCode;
         Message = message;
+        Details = details;
     }
     public static Result Success() => new();
-    public static Result Failed(ErrorCode errorCode, string? message = null) => new(errorCode, message);
+    public static Result Failed(
+        ErrorCode errorCode,
+        string? message = null,
+        Dictionary<string, string>? details = null
+    ) => new(errorCode, message, details);
 
-    public static Result IsComplited() => Success();
     public static Result CompletedOperation() => Success();
-    public static Result FailedOperation(ErrorCode errorCode, string? message = null) => Failed(errorCode, message);
+    public static Result FailedOperation(
+        ErrorCode errorCode,
+        string? message = null,
+        Dictionary<string, string>? details = null
+    ) => Failed(errorCode, message, details);
 }
 public sealed class Result<T> : IResultBase
 {
-    public Result(T value)
-    {
-        Value = value;
-    }
     public T Value { get; }
     public bool IsSuccess => Error is null;
+    public bool IsComplited => IsSuccess;
     public ErrorCode? Error { get; }
     public string? Message { get; }
-    internal Result(T value, ErrorCode? error, string? message)
+    public Dictionary<string, string>? Details { get; }
+    internal Result(
+        T value,
+        ErrorCode? error,
+        string? message,
+        Dictionary<string, string>? details = null
+    )
     {
         Value = value;
         Error = error ?? ErrorCode.Unknown;
         Message = message;
+        Details = details;
     }
-    public static Result<T> Success(T value) => new(value);
-    public static Result<T> Failed(ErrorCode errorCode, string? message = null) => new(default!, errorCode, message);
+    public static Result<T> Success(T value) => new(value, null, null, null);
+    public static Result<T> Failed(
+        ErrorCode errorCode,
+        string? message = null,
+        Dictionary<string, string>? details = null
+    ) => new(default!, errorCode, message, details);
 
-    public static implicit operator Result(Result<T> result) => new(result.Error, result.Message);
-    public static implicit operator Result<T>(Result result) => new(default!, result.Error, result.Message);
+    public static implicit operator Result(Result<T> result) => new(result.Error, result.Message, result.Details);
+    public static implicit operator Result<T>(Result result) => new(default!, result.Error, result.Message, result.Details);
 
-    public static Result<T> IsComplited(T value) => Success(value);
     public static Result<T> CompletedOperation(T value) => Success(value);
-    public static Result<T> FailedOperation(ErrorCode errorCode, string? message = null) => Failed(errorCode, message);
+    public static Result<T> FailedOperation(ErrorCode errorCode, string? message = null, Dictionary<string, string>? details = null) => Failed(errorCode, message, details);
 }
