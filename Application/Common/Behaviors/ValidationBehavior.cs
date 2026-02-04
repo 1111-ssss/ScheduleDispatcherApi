ac49cf599ebc001c8c32fcd2e337f33a0f55a83f
1111-ssss <gurskievgeni@gmail.com>
2026-02-04 09:22:20 +0300

    feat(result): add details

--- Application/Common/Behaviors/ValidationBehavior.cs
+++ Application/Common/Behaviors/ValidationBehavior.cs
@@ -2,47 +2,56 @@ using Domain.Abstractions.Result;
 using FluentValidation;
 using MediatR;
 
-namespace Application.Common.Behaviors
+namespace Application.Common.Behaviors;
+
+public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+where TRequest : IRequest<TResponse>
+where TResponse : class, IResultBase
 {
-    public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
-    where TRequest : class
-    where TResponse : class, IResultBase
+    private readonly IEnumerable<IValidator<TRequest>> _validators;
+    public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
     {
-        private readonly IEnumerable<IValidator<TRequest>> _validators;
-        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+        _validators = validators;
+    }
+    public async Task<TResponse> Handle(
+        TRequest request,
+        RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken
+    )
+    {
+        if (!_validators.Any())
+            return await next();
+
+        var context = new ValidationContext<TRequest>(request);
+        var validationResults = await Task.WhenAll(
+            _validators.Select(v => v.ValidateAsync(context, cancellationToken))
+        );
+
+        var failures = validationResults
+            .SelectMany(r => r.Errors)
+            .Where(f => f != null)
+            .ToList();
+
+        if (!failures.Any())
+            return await next();
+
+        Dictionary<string, string> details = new();
+        failures.ForEach(f => details.Add(f.CustomState?.ToString() ?? "Unknown", f.ErrorMessage));
+
+        var mainError = failures.First();
+        var errorCode = mainError.CustomState as ErrorCode?
+            ?? ErrorCode.BadRequest;
+
+        string errorMessage = mainError.ErrorMessage;
+
+        if (typeof(TResponse).IsGenericType && 
+            typeof(TResponse).GetGenericTypeDefinition() == typeof(Result<>))
         {
-            _validators = validators;
+            return (TResponse)typeof(TResponse)
+                .GetMethod(nameof(Result.Failed))!
+                .Invoke(null, new object[] { errorCode, errorMessage, details })!;
         }
-        public async Task<TResponse> Handle(
-            TRequest request,
-            RequestHandlerDelegate<TResponse> next,
-            CancellationToken cancellationToken
-        )
-        {
-            if (!_validators.Any())
-                return await next();
 
-            var context = new ValidationContext<TRequest>(request);
-            var validationResults = await Task.WhenAll(
-                _validators.Select(v => v.ValidateAsync(context, cancellationToken))
-            );
-
-            var failures = validationResults
-                .SelectMany(r => r.Errors)
-                .Where(f => f != null)
-                .ToList();
-
-            if (!failures.Any())
-                return await next();
-
-            var firstError = failures.First();
-            var errorCode = firstError.CustomState as ErrorCode?
-                ?? ErrorCode.BadRequest;
-
-            string errorMessage = firstError.ErrorMessage;
-
-            var responseResult = Result.Failed(errorCode, errorMessage);
-            return (TResponse)(object)responseResult;
-        }
+        return (TResponse)(object)Result.Failed(errorCode, errorMessage, details);
     }
 }
