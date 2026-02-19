776cf9f5835f2160a11a58fd4c177111d0d104a7
1111-ssss <gurskievgeni@gmail.com>
2026-02-19 12:31:23 +0300

    fix(saveWorkload): add fields

--- src/Application/Features/Dispatcher/Commands/SaveWorkload/SaveWorkloadCommand.cs
+++ src/Application/Features/Dispatcher/Commands/SaveWorkload/SaveWorkloadCommand.cs
@@ -5,5 +5,9 @@ using MediatR;
 namespace Application.Features.Dispatcher.SaveWorkload;
 
 public record SaveWorkloadCommand(
+    string Lesson,
+    string Teacher,
+    string Group,
+    int Semester,
     WorkloadSummaryDTO WorkloadSummary
 ) : IRequest<Result>;
