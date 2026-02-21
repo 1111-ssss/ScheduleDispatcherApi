1cd6f93b7445c053b5825217f5cc741c265f95ea
1111-ssss <gurskievgeni@gmail.com>
2026-02-21 11:54:39 +0300

    refact(user): remove create method

--- src/Domain/Entities/User.cs
+++ src/Domain/Entities/User.cs
@@ -6,18 +6,4 @@ public class User
     public string Username { get; set; } = default!;
     public string PasswordHash { get; set; } = default!;
     public string Role { get; set; } = default!;
-
-    public static User Create(
-        string username,
-        string password,
-        string role
-    )
-    {
-        return new User
-        {
-            Username = username,
-            PasswordHash = password,
-            Role = role
-        };
-    }
 }
