1a5ca25cd28fd1bab3bb173c15c11fc62bb4b755
1111-ssss <gurskievgeni@gmail.com>
2026-02-21 12:49:31 +0300

    refact(dbContext): dbset edit

--- src/Infrastructure/DataBase/Context/AppDbContext.cs
+++ src/Infrastructure/DataBase/Context/AppDbContext.cs
@@ -1,14 +1,31 @@
+using Infrastructure.src.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.DataBase.Context;
 
-public class AppDbContext : DbContext
+public partial class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options)
     : base(options) { }
 
+    public virtual DbSet<Classroom> Classrooms { get; set; }
+    public virtual DbSet<Curator> Curators { get; set; }
+    public virtual DbSet<Employer> Employers { get; set; }
+    public virtual DbSet<Exam> Exams { get; set; }
+    public virtual DbSet<Group> Groups { get; set; }
+    public virtual DbSet<Okr> Okrs { get; set; }
+    public virtual DbSet<Removal> Removals { get; set; }
+    public virtual DbSet<Schedule> Schedules { get; set; }
+    public virtual DbSet<Specialty> Specialties { get; set; }
+    public virtual DbSet<Subject> Subjects { get; set; }
+    public virtual DbSet<SubjectTeacher> SubjectTeachers { get; set; }
+
+    public virtual DbSet<SubjectTeacherSchedule> SubjectTeacherSchedules { get; set; }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        OnModelCreatingPartial(modelBuilder);
     }
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 }
